f82bb97200131678e62b1d519752a5fcf30c6484
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2021-02-21 23:53:05 +0600

    react start

---
+++ milestone5:Intermidiate-JavaScript,API/timeout.js
@@ -0,0 +1,16 @@
+function doSomething(){
+    console.log(1000000);
+}
+console.log(55555);
+console.log(22222);
+setTimeout (() => {
+    console.log('lazy and stedy');
+}, 4000);
+console.log(22222);
+setInterval (() => {
+    console.log('Interval');
+}, 1000);
+console.log(22222);
+console.log(22222);
+console.log(22222);
+
